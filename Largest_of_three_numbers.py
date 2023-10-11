f684fda6b4d890043b6efcc04a9428de73021f1e
Ashly Elizebath Joseph <108012500+AshlyElizebathJoseph@users.noreply.github.com>
2023-10-11 21:25:53 +0530

    Create Largest_of_three_numbers.py

---
+++ Largest_of_three_numbers.py
@@ -0,0 +1,9 @@
+a=int(input("Enter the number a:"))
+b=int(input("Enter the number b:"))
+c=int(input("Enter the number c:"))
+if(a>b) and (b>c):
+  print("a is the largest")
+elif(b>a) and (b>c):
+  print("b is the largest")
+else:
+  print("c is the largest")
