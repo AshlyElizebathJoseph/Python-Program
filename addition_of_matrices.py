06bf59648537ef5717e31bff976e59a3644a2bc6
Ashly Elizebath Joseph <108012500+AshlyElizebathJoseph@users.noreply.github.com>
2023-10-14 11:05:12 +0530

    Create addition_of_matrices.py

---
+++ addition_of_matrices.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+mat1=np.array([[1,2],[3,4],[7,5]])
+mat2=np.array([[2,2],[3,8],[5,9]])
+
+mat_sum=np.add(mat1,mat2)
+print("\n Matrix1: ")
+print(mat1)
+print("\n Matrix2: ")
+print(mat2)
+print("\n Sum of Matrices: ")
+print(mat_sum)
