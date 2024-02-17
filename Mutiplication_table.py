68f281f9770ba974da89b239245d6f4b02d9612b
Ashly Elizebath Joseph <108012500+AshlyElizebathJoseph@users.noreply.github.com>
2024-02-17 21:22:12 +0530

    Update Mutiplication_table.py

--- Mutiplication_table.py
+++ Mutiplication_table.py
@@ -1,4 +1,14 @@
-num=int(input("Enter the number:"))
-print("The multiplication table of:",num)
-for count in range(1,11):
-  print(num,'x',count,'=',num*count)
+def generate_multiplication_table(number, num_terms):
+    multiplication_table = []
+    for i in range(1, num_terms + 1):
+        multiplication_table.append(number * i)
+    return multiplication_table
+
+# Example usage:
+number = int(input("Enter the number for multiplication table: "))
+num_terms = int(input("Enter the number of terms in the table: "))
+
+table = generate_multiplication_table(number, num_terms)
+print(f"The multiplication table of {number} until {num_terms} term(s) is:")
+print(table)
+
